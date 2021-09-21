8b2a47d82d7a3cfba4b941b256dcbc01eef184b0
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-09-21 00:15:21 +0000

    ..

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,71 @@
-import React from "react";
+import { bottom } from "@popperjs/core";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [tasks, setTasks] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container text-center mt-5">
+				<h1>To do</h1>
+				<div className="input-group mb-3 text-center d-block ">
+					<div className="form-group p-3 .bg-info">
+						<label htmlFor="exampleFormControlTextarea1"></label>
+						<textarea
+							className="form-control"
+							id="exampleFormControlTextarea1"
+							rows="3"
+							value={tarea}
+							onChange={e => {
+								setTarea(e.target.value);
+							}}
+							onKeyDown={e => {
+								if (e.keyCode == "13") {
+									let MostrarLista = [];
+									for (let i = 0; i < tasks.length; i++) {
+										MostrarLista.push(tasks[i]);
+									}
+									MostrarLista.push(tarea);
+									setTasks(MostrarLista);
+									setTarea("");
+								}
+							}}></textarea>
+					</div>
+					<ul className="list-group">
+						{tasks.map((item, index) => {
+							return (
+								<>
+									<li
+										key={index}
+										className="list-group-item list-group-item-info">
+										{item}
+										<i className="fas fa-times float-end"></i>
+										removeItem(e){" "}
+										{this.props.removeTodo(item)}
+										{this.props.todos.map(todo => {
+											return (
+												<li
+													onClick={() => {
+														this.removeItem(todo);
+													}}
+													key={todo}>
+													{todo}
+												</li>
+											);
+										})}
+									</li>
+								</>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
